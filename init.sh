7bfa248328e9f8eec37287d57a7fdb2040001404
owenliang <owenliang1990@gmail.com>
2020-03-14 16:17:41 +0800

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-# node瓶颈是conntrack
 # 下面2项令TCP窗口和状态追踪更加宽松
 sysctl -w net.netfilter.nf_conntrack_tcp_be_liberal=1
 sysctl -w net.netfilter.nf_conntrack_tcp_loose=1 
@@ -10,8 +8,6 @@ sysctl -w net.netfilter.nf_conntrack_max=3200000
 sysctl -w net.netfilter.nf_conntrack_buckets=1600512
 sysctl -w net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
 
-
-# pod瓶颈是TCP协议栈
 sysctl -w net.ipv4.tcp_timestamps=1 # 与tw_reuse一起用
 sysctl -w net.ipv4.tcp_tw_reuse=1 # 仅作用于客户端，允许复用TIME_WAIT端口（与tcp_rw_recycle不同，该选项不受NAT场景下时间戳不一致问题影响）
 sysctl -w net.ipv4.ip_local_port_range="5120 65000" # 端口范围
@@ -21,5 +17,6 @@ sysctl -w net.ipv4.tcp_max_syn_backlog=10240
 sysctl -w net.core.somaxconn=10240
 sysctl -w net.ipv4.tcp_syncookies=1
 
+# 关闭透明大页
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
